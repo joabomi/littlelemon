3753fc386147befe7cb6e2e511381542948f205c
MaSaKaBeS Frimost <MaSaKaBeS>
2025-01-31 13:11:29 +0100

    security access implemented using djoser library

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -7,6 +7,9 @@ class Booking(models.Model):
     bookingDate = models.DateTimeField()
     
 class Menu(models.Model):
-    title = models.CharField(max_length=255)
+    tittle = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
@@ -7,14 +9,18 @@ from .serializers import MenuSerializer, BookingSerializer
 def index(request):
     return render(request, 'index.html', {})
 
+@permission_classes([IsAuthenticated])
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-    
+
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
